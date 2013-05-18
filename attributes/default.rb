24fd5d8dee9120972edef3cf1e25e182a8ecb6ee
Hector Castro <hectcastro@gmail.com>
2013-05-18 18:57:26 -0400

    Bumped s3cmd version to 1.5.0-alpha3.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["s3cmd"]["version"]                 = "1.1.0-beta3"
+default["s3cmd"]["version"]                 = "1.5.0-alpha3"
 default["s3cmd"]["dir"]                     = "/opt/s3cmd"
 default["s3cmd"]["url"]                     = "https://github.com/s3tools/s3cmd/archive/v#{node["s3cmd"]["version"]}.tar.gz"
 default["s3cmd"]["checksum"]                = "bd14cc7593ce0e253014fe7e9e384a539c101bde4d5c6e28f048f8ae80035a30"
